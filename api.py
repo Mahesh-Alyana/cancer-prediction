46eb4285ed3b4b356c6672679b2b164626439390
Mahesh Alyana <maheshalyana@Mahesh-MacBook-Air.local>
2023-07-26 20:30:44 +0530

    removing unnecessary packages

--- api.py
+++ api.py
@@ -1,15 +1,12 @@
 import io
 from fastapi.responses import FileResponse, StreamingResponse
-from flask import Flask, request, render_template, jsonify
 from werkzeug.utils import secure_filename
 import pandas as pd
 import json
-from flask_cors import CORS
 import tensorflow as tf
 import json 
 import os
 from model_definition import SegmentationModel 
-import cv2
 import json
 from matplotlib import pyplot as plt
 import numpy as np
